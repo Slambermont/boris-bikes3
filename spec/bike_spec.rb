18c43ca80393c64518f85625010ed140e7d36b50
Salome Lambermont <salome.lambermont@gmail.com>
2018-04-20 15:11:04 +0100

    Add a function to report broken bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,12 @@
 require 'bike'
 
 describe Bike do
-  it { is_expected.to respond_to :working? }
+  it { is_expected.to respond_to :working }
+
+  describe '#report_broken' do
+    it 'should set the bike to "broken"' do
+      subject.report_broken
+      expect(subject.working).to eq false
+    end
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -19,7 +19,7 @@ end
   describe '#release_bike' do
     it 'releases a working bike' do
       bike = Bike.new
-      expect(bike).to be_working
+      expect(bike.working).to eq true
     end
     it 'raises an error when dock is empty' do
       expect { subject.release_bike }.to raise_error("No bikes available")
@@ -36,6 +36,11 @@ end
       subject.dock(bike)
       expect { DockingStation::DEFAULT_CAPACITY.times { subject.dock(bike) } }.to raise_error("Dock is full")
     end
+#    it 'Allows the user to tell the station that the bike is broken' do
+#      subject.dock(Bike.new, 'broken')
+#      expect(subject.docks).to eq [Bike.new 'broken']
+#    end
+
   end
 
   describe '#bike' do
